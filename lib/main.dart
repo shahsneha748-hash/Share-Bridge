e77582f47fd13b9061af3b202570ce2cace0d107
firstnamecheten <firstnamecheten@yahoo.com>
2026-05-25 00:30:43 +0545

    Work in progress on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:sharebridge/forgot_password_screen.dart';
-import 'package:sharebridge/login_screen.dart';
-import 'package:sharebridge/notification_screen.dart';
-import 'package:sharebridge/signup_screen.dart';
-import 'package:sharebridge/splash_screen.dart';
+// import 'package:sharebridge/view/forgot_password_screen.dart';
+// import 'package:sharebridge/view/login_screen.dart';
+// import 'package:sharebridge/view/notification_screen.dart';
+// import 'package:sharebridge/view/signup_screen.dart';
+import 'package:sharebridge/view/splash_screen.dart';
 
 void main() {
   runApp(MyHomePage());
@@ -18,7 +18,6 @@ class MyHomePage extends StatelessWidget {
       title: "Share-Bridge",
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
-
     );
   }
 }
